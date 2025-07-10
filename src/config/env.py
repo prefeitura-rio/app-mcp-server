3cd0aa1f0e117619c0cce64fae4de7028ac8cbfe
d116626 <d116626@gmail.com>
2025-07-10 13:08:47 -0300

    chore: add a key to run local

--- src/config/env.py
+++ src/config/env.py
@@ -1,3 +1,11 @@
+import os
 from src.utils.infisical import getenv_or_action
+
+# if file .env exists, load it
+if os.path.exists("src/config/.env"):
+    import dotenv
+
+    dotenv.load_dotenv(dotenv_path="src/config/.env")
+
 
 VALID_TOKENS = getenv_or_action("VALID_TOKENS")
