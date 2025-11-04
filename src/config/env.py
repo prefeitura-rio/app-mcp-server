029ac6549b96ee94cf48dbc0b1585330f5d0c4cc
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-11-04 12:33:59 -0300

    incidentes hidricos

--- src/config/env.py
+++ src/config/env.py
@@ -41,7 +41,8 @@ LINK_BLACKLIST = getenv_or_action("LINK_BLACKLIST", default="").split(",")
 
 # Configuração para temas válidos da ferramenta de equipamentos
 EQUIPMENTS_VALID_THEMES = getenv_or_action(
-    "EQUIPMENTS_VALID_THEMES", default="cultura,saude,educacao,geral,pontos_de_apoio"
+    "EQUIPMENTS_VALID_THEMES",
+    default="cultura,saude,educacao,geral,incidentes_hidricos",
 ).split(",")
 
 # PGM API Configuration
